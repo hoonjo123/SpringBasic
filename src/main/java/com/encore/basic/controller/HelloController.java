f3a4384d9944f0c12115a321efe197f5dcdd84c5
hoonjo123 <johoon030@gmail.com>
2024-01-12 14:27:38 +0900

    first

--- src/main/java/com/encore/basic/controller/HelloController.java
+++ src/main/java/com/encore/basic/controller/HelloController.java
@@ -1,51 +1,96 @@
 package com.encore.basic.controller;
 
+import com.encore.basic.domain.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
-@Controller //http 통신을 원활하게!
-//RestController가 있으면 프론트가 따로 있나보다~ 라고 생각하기
-//모든 요청에 ResponseBody를 붙이고 싶으면, RestController사용
-
-//클래스차원에서 url경로를 지정하고 싶다면 @RequestMapping을 클래스 위에 선언하면서 경로지정.
-//사용자 요청에 대해 분기처리가가능한 기능
-//@responseBody가 없고, return타입이 String이면 templates 밑에 html파일 리턴
-// data만을 return할 때에는 @ResponseBody를 붙인다.
-
+@Controller //http 통신을 쉽게 하게 해주는 어노테이션
+//클래스 차원에서 url 경로를 지정하고 싶다면 @RequestMapping을 클래스 위에 선언하면서 경로 지정.
 @RequestMapping("hello")
+//@RestController //모든 요청에 ResponseBody를 붙이고 싶다면, RestController 사용
+//data만 줘야할 경우, 내부 함수에 모두 @ResponseBody가 붙는다
+//이 경우 String으로 화면 return이 불가하고, 데이터만 반환할 수 있다.
 public class HelloController {
+    //ReponseBody가 없고, return타입이 String이면 templates밑에 html파일 리턴
+    //data만을 return할 때는 @ResponseBody를 붙인다.
+    //@ResponseBody가 붙었을 때 반환 타입이 객체라면, JSON 형식으로 반환한다.
+//    @RequestMapping(value = "string", method = RequestMethod.GET) //@GetMapping("string")와 같은 의미
+    @GetMapping("string")
+    @ResponseBody
+    public String helloString(){
+        return "hello_string";
+    }
+
+    @GetMapping("json")
+    @ResponseBody
+    public Hello helloJson(){
+        Hello hello = new Hello();
+        hello.setName("kamill");
+        hello.setEmail("ka@naver.com");
+        hello.setPassword("1234");
+        System.out.println(hello);
+        return hello;
+    }
+
     @GetMapping("screen")
-    public String helloScreen() {
-        return "screen"; //hello_String.html 파일을 찾으러 templates로 여행을 떠나지만 아무것도 없어서 오류남. -> @ResponseBody를 붙여보자
-        // template might not exist or might not be accessible b ...어쩌고 저쩌고..
+    public String helloScreen(){
+        return "screen";
     }
+
+    //1)parameter방식 : ?키1=밸류1&키2=밸류2
+    //2)pathVariable방식 : localhost:8080/hello/screen-model/hongildong  ex) localhost:8080/member/hongildong
+    // -> 서버의 특정 자원의 값을 가져올 때 사용. 요즘은 2번 방식이 더 많다.
     @GetMapping("screen-model-param")
-    //?name=honggildong의 파라미터방식으로 호출방식
+    //@RequestParam(파라미터 호출방식) ==> localhost:8080/member-model-param?name=hongildong 방식으로 호출
     public String helloScreenModelParam(@RequestParam(value = "name")String inputName, Model model){
-        //화면에 데이터를 넘기고 싶을때는 model객체를 사용
-        //모델의 key:value형식으로 전달
-        model.addAttribute("mydata",inputName); //jsp,thyme leaf와 같은
-        // 템플릿 엔진을 통해 화면에 자바문법을 동적으로 구현가능
+        //화면에 data를 넘기고 싶을 때는 model 객체 사용
+        //model에 key:value 형식으로 전달
+        model.addAttribute("myData", inputName);
         return "screen";
+    }
 
-        //parameter 방식 : ?키 =밸류 방식 localhost:8080/hello/screen-model?name="honggildong"
-        //pathvariable 방식 localhost:8080/hello/screen-model/1..2....3......특정한아이디값
+    //pathVariable방식은 url을 통해 자원의 구조를 명확하게 표현할 수 있어,
+    // 좀 더 RestFul API 디자인에 적합하다.
+    @GetMapping("screen-model-path/{id}")
+    //localhost:8080/member-model-path/1 방식으로 호출
+    public String helloScreenModelPath(@PathVariable int id, Model model){
+        model.addAttribute("myData", id);
+        return "screen";
+    }
 
-
-//        localhost:8080/member?id=1
-//        localhost:8080/member/1
+    //form 태그로 x-www-urlencoded 데이터 처리
+    @GetMapping("form-screen")
+    public String formScreen(){
+        return "hello-form-screen";
+    }
+    @PostMapping("form-post-handle")
+    //form 태그를 통한 body의 데이터 형태가 key1=value1, key2=value2
+    //GET요청의 파라미터 호출방식과 동일하기 때문에 @RequestParam 사용
+    @ResponseBody
+    public String formPostHandle(@RequestParam(value = "name")String name,
+                                 @RequestParam(value = "email")String email,
+                                 @RequestParam(value = "password")String password){
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
+        System.out.println("password = " + password);
+        return "정상처리";
     }
 
 
-    //path variable 방식은 url을 통해 자원의 구조를 명확하게 표현할 수 있어 좀 더 RestFulAPI디자인에 적합하다.
-    //RestFul이란? 현대적인 아키텍쳐에 적합한....음?
-    @GetMapping("screen-model-path/{id}")
-    public String helloScreenModelPath(@PathVariable int id, Model model){
-        model.addAttribute("mydata",id);
-        return "screen";
-
-
-
+    //json데이터 처리
+    @GetMapping("json-screen")
+    public String jsonScreen(){
+        return "hello-json-screen";
+    }
+    @PostMapping("/json-post-handle1")
+    @ResponseBody
+    //@RequestBody는 json으로 post 요청이 들어왔을 때 body에서 데이터를 꺼내기 위해 사용
+    public String jsonPostHandle1(@RequestBody Map<String, String> body){
+        System.out.println("이름 : " + body.get("name"));
+        System.out.println("email :" + body.get("email"));
+        System.out.println("password : " + body.get("password"));
+        return "정상처리";
     }
 }
